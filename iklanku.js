a7fca29542103c2e30962f8793fac5b1bcda1fde
safelinkwebid <71550781+safelinkwebid@users.noreply.github.com>
2021-11-29 14:01:10 +0700

    Create iklanku.js

---
+++ iklanku.js
@@ -0,0 +1,6 @@
+// JavaScript Document //<br /><br />
+ads = new Array(4);<br /><br />
+ads [0] = ‘<div class=”ads-box” “item”><a href=”http://caragratis.net” target=”blank”><img src=”http://caragratis.net/file/img/banner728X90.png”></a><p class=”ad”><a href=”http://caragratis.net” target=”blank”><span><font color=”#000000″ size=”1″ face=”Arial, Helvetica, sans-serif”>AdChoices</font></span></a></p><br />
+</div><br />
+<p>’;<br /><br />
+view = Math.floor(Math.random() * 1); document.write(ads[view]);
